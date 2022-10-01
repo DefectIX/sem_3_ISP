1b03b520a287e096bd17b0aa3e492df7e57b99f1
Даниил Кочергов <15350066@study.bsuir.by>
2022-10-01 22:42:58 +0300

    Added laba 6 files

---
+++ 153502_Kochergov_Lab6/153502_Kochergov_Lab6/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _153502_Kochergov_Lab6
+{
+	public class Employee
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public bool IsOutsourcer { get; set; }
+		public override string ToString()
+		{
+			return $"Name:{Name} Age:{Age} Is outsourcer:{IsOutsourcer}";
+		}
+	}
+}
---
+++ 153502_Kochergov_Lab6/FileService/FileService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using _153502_Kochergov_Lab6;
+
+namespace _FileService
+{
+	public class FileService<T> : IFileService<T> where T : class
+	{
+		public IEnumerable<T> ReadFile(string fileName)
+		{
+			return JsonSerializer.Deserialize<IEnumerable<T>>(File.ReadAllText(fileName));
+		}
+
+		public void SaveData(IEnumerable<T> data, string fileName)
+		{
+			File.WriteAllText(fileName, JsonSerializer.Serialize(data));
+		}
+	}
+}
