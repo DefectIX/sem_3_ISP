245dbcf743acb9ef3bab4536b7a35d45a29c3e2a
Даниил Кочергов <15350066@study.bsuir.by>
2022-10-02 02:41:02 +0300

    Finished laba 6

--- 153502_Kochergov_Lab6/153502_Kochergov_Lab6/Program.cs
+++ 153502_Kochergov_Lab6/153502_Kochergov_Lab6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -8,10 +9,22 @@ namespace _153502_Kochergov_Lab6
 	{
 		static void Main(string[] args)
 		{
+			List<Employee> list = new()
+			{
+				new() { Name = "Name1", Age = 34, IsOutsourcer = true },
+				new() { Name = "Name3", Age = 35, IsOutsourcer = false },
+				new() { Name = "Name2", Age = 32, IsOutsourcer = true },
+				new() { Name = "Name5", Age = 31, IsOutsourcer = false },
+				new() { Name = "Name4", Age = 31, IsOutsourcer = true }
+			};
+
 			Assembly assembly = Assembly.LoadFile(Path.GetFullPath("FileService.dll"));
 			Type type = assembly.GetTypes()[0];
 			var fileService = Activator.CreateInstance(type.MakeGenericType(typeof(Employee))) as IFileService<Employee>;
-			
+
+			fileService.SaveData(list, "list.json");
+			var list2 = fileService.ReadFile("list.json");
+			Console.WriteLine(string.Join("\n", list2));
 		}
 	}
 }
