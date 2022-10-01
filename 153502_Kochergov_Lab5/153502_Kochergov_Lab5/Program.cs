db21eff1db95e14841e8f267c44bde5b27e59f94
Даниил Кочергов <15350066@study.bsuir.by>
2022-10-01 11:24:18 +0300

    Upd

--- 153502_Kochergov_Lab5/153502_Kochergov_Lab5/Program.cs
+++ 153502_Kochergov_Lab5/153502_Kochergov_Lab5/Program.cs
@@ -12,17 +12,6 @@ namespace _153502_Kochergov_Lab5
 	{
 		static void Main(string[] args)
 		{
-			var query = from x in Enumerable.Range(1, 10)
-						from y in Enumerable.Range(1, 10)
-						select $"{x * y} " + ((y == 10) ? "\n" : "") + ((y != 10 && x * y / 10 == 0) ? " " : "");
-
-			string res = "";
-			for (int i = 1; i <= 10; i++)
-				for (int j = 1; j <= 10; j++)
-					res += $"{i * j} " + ((j == 10) ? "\n" : "") + ((j != 10 && i * j / 10 == 0) ? " " : "");
-
-			//Console.WriteLine(string.Join("", query));
-			Console.WriteLine(res);
 			ISerializer serializer = new Serializer();
 			List<Station> list = new List<Station>();
 			list.Add(new Station(new LuggageOffice { ContainersNumber = 1, PhoneNumber = "111-11-11", WorkersNumber = 12}));
